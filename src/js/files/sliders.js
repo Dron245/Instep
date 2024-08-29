6c82809fd6c8b35f6803b1b8761d3bd7039470d6
Dron245 <Dron245@yandex.ru>
2024-08-29 20:29:03 +0300

    2908-2029

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -38,7 +38,7 @@ function initSliders() {
 			observeParents: true,
 			slidesPerView: 1,
 			spaceBetween: 0,
-			// autoHeight: true,
+			autoHeight: true,
 			speed: 800,
 
 			loop: true,
@@ -172,7 +172,80 @@ function initSliders() {
 		
 					}
 				});
-			}
+	}
+
+	if (document.querySelector('.sertificates__slider')) { //Указываем класс нужного слайдера
+		//Создаем слайдер
+				new Swiper('.sertificates__slider', { //Указываем класс нужного слайдера
+						//Подключаем модули слайдера
+						//для конкретного случая
+					modules: [Pagination],
+					observer: true,
+					observeParents: true,
+					slidesPerView: 3,
+					spaceBetween: 20,
+					// autoHeight: true,
+					speed: 800,
+		
+					loop: true,
+		
+					/*
+					// Эффекты
+					autoplay: {
+						delay: 3000,
+						disableOnInteraction: false,
+					},
+					*/
+		
+					// Пагинация
+					
+					pagination: {
+						el: '.sertificates__pagination',
+						clickable: true,
+					},
+					
+		
+					// Скроллбар
+					/*
+					scrollbar: {
+						el: '.swiper-scrollbar',
+						draggable: true,
+					},
+					*/
+		
+					// Кнопки "влево/вправо"
+					// navigation: {
+					// 	prevEl: '.swiper-button-prev',
+					// 	nextEl: '.swiper-button-next',
+					// },
+					/*
+					// Брейкпоинты
+					breakpoints: {
+						640: {
+							slidesPerView: 1,
+							spaceBetween: 0,
+							autoHeight: true,
+						},
+						768: {
+							slidesPerView: 2,
+							spaceBetween: 20,
+						},
+						992: {
+							slidesPerView: 3,
+							spaceBetween: 20,
+						},
+						1268: {
+							slidesPerView: 4,
+							spaceBetween: 30,
+						},
+					},
+					*/
+					// События
+					on: {
+		
+					}
+				});
+	}
 }
 //Скролл на базе слайдера (по классу swiper scroll для оболочки слайдера)
 function initSlidersScroll() {
